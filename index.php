a683d06de5e2cb52810dc235ab802d501a2d7010
AleRojas27 <alejandro.rrc99@hotmail.com>
2020-07-31 17:58:31 -0500

    Pagina Principal de Buscalab terminada 1.0

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="vista/assets/css/general6.css">
+    <link rel="stylesheet" href="vista/assets/css/navbar6.css">
+    <link rel="stylesheet" href="vista/assets/css/buscadorPrincipal.css">
+    <link rel="stylesheet" href="vista/assets/css/busquedaRapida.css">
+    <link rel="stylesheet" href="vista/assets/css/descripcionStartUp.css">
+    <link rel="stylesheet" href="vista/assets/css/restoPagina.css">
+    <link rel="stylesheet" href="vista/assets/css/piePagina6.css">
+    <link rel="stylesheet" href="vista/assets/css/all.min.css">
+
+
+    <title>Proyecto 6</title>
+  </head>
+  <body>
+
+    <?php
+    
+    include "vista/modulos/paginaPrincipal.php";
+
+    ?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ vista/modulos/paginaPrincipal.php
@@ -0,0 +1,365 @@
+<!--Navbar6-->
+
+<nav class="container-fluid estiloContainerNav shadow">
+
+    <div class="row">
+
+        <div class="col-3">
+
+            <h4 class="colorBuscalab pl-4">
+            
+                <span> <img class="img-fluid" src="vista/assets/img/logo.png" width="90px" height="90px"> </span>
+                <b> Busca<span class="colorBuscalabGris">lab </span> </b>
+            
+            </h4>
+
+        </div>
+
+        <div class="col-4">
+        
+        </div>
+
+        <div class="col-5">
+        
+            <ul id="horizontal" class="float-right">
+
+                <li>
+
+                    <button class="btn btn-link text-muted buttonNav">
+
+                        <i class="fas fa-user"></i>
+                        Iniciar sesión
+                    
+                    </button>
+
+                </li>
+
+                <li>
+                
+                    <button class="btn btn-link text-muted buttonNav">
+
+                    <i class="fas fa-calendar-alt"></i>
+                    Registrarse
+
+                    </button>
+
+                </li>
+
+            </ul>
+
+        </div>
+    
+    </div>
+
+</nav>
+
+<!--Buscador principal-->
+
+<div class="container-fluid estiloContainerBusPrin shadow">
+
+    <div class="row pt-5">
+
+        <div class="col-2">
+
+
+        </div>
+
+        <div class="col-8">
+
+            <div class="form-inline" id="estiloSearch">
+                <input type="text" class="form-control inputForm" type="search" id="search">
+                <i class="fas fa-search mr-1 iconSearch"></i> 
+            </div>
+        </div>
+
+        <div class="col-2">
+
+
+        </div>
+
+    </div>
+
+    <p class="text-center textBusPrin pt-5">Encuentra tu laboratorio ideal</p>
+
+</div>
+
+<!--Busqueda rapida-->
+
+<div class="container-fluid estiloContainerBusRap text-center text-muted">
+
+    <h3 class="pt-4">Búsqueda rápida</h3>
+
+    <div class="row">
+
+        <div class="col-3">
+
+            <img src="vista/assets/img/carilla.png" width="250px" height="210px">
+            <p class="textBusRap pt-2">Carilla</p>
+
+        </div>
+
+        <div class="col-3">
+
+            <img src="vista/assets/img/prostodonciaTotal.jpg" width="250px" height="210px">
+            <p class="textBusRap pt-2">Prostodoncia</p>
+
+        </div>
+
+        <div class="col-3">
+
+            <img src="vista/assets/img/corona.jpg" width="250px" height="210px">
+            <p class="textBusRap pt-2">Corona</p>
+        
+        </div>
+
+        <div class="col-3">
+
+            <img src="vista/assets/img/retenedor.jpg" width="250px" height="210px">
+            <p class="textBusRap pt-2">Retenedores</p>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--Descripcion startup-->
+
+<div class="container-fluid text-center estiloContainerDesStart shadow">
+
+    <h1 class="pt-5 pb-2 colorNegroClaro">PRIMER Y <span class="colorBuscalabGeneral"> ÚNICO </span> BUSCADOR DE LABORATORIOS DENTALES</h1>
+
+    <hr>
+
+    <p class="pDesStart pt-5 pb-4 colorNegroClaro">
+
+        Mejoramos la comunicación, conectamos pagos, pedidos y envíos entre
+        <br>dentistas/clínicas y técnicos dentales/laboratorios de la <span class="textDesRapNaranja"> Ciudad de México </span>    
+
+    </p>
+
+</div>
+
+<!--Resto Pagina Principal-->
+
+<div class="containerfluid text-center text-muted">
+
+    <div class="row">
+
+        <div class="col-6" id="paddingText">
+
+            <h1 class="colorBuscalabGeneral">
+
+                Busca y filtra cientos de
+                <br> laboratorios dentales
+                <span><i class="fas fa-teeth-open mr-2"></i></span>
+            
+            </h1>
+
+            <p class="text-muted textP">Busca algún material o trabajo (corona, metal porcelana,
+            <br> prostodoncia, emax...), filtra los resultados y cotiza tu <span class="colorGrisOscuro textP"> <br> laboratorio dental ideal.</span> </p>
+
+        </div>
+
+        <div class="col-6" id="paddingImg">
+
+            <img src="vista/assets/img/paso1.gif" whidth="500px" height="500px">
+
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+
+        <div class="col-6" id="paddingText">
+
+            <h1 class="colorBuscalabGeneral">
+
+                Selecciona un laboratorio,
+                <br> llena la orden y confírmala
+                <br> <i class="fas fa-check-circle"></i>
+            
+            </h1>
+
+            <p class="textP">Cuando selecciones tu laboratorio dental ideal,
+            <br> proporciona todos los detalles posibles a la orden, <span class="colorGrisOscuro textP"> incluso
+            <br> puedes agregar fotos. </span> Buscalab recogerá la impresión en
+            <br> tu clínica dental.
+            </p>
+
+        </div>
+
+        <div class="col-6" id="paddingImg">
+
+            <img src="vista/assets/img/paso2.gif" whidth="500px" height="500px">
+
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+
+        <div class="col-6" id="paddingText">
+
+            <h1 class="colorBuscalabGeneral">
+
+                Selecciona un metodo de
+                <br> pago y genera tu orden
+                <i class="far fa-credit-card"></i>
+            
+            </h1>
+
+            <p class="textP">Puedes pagar con <span class="colorGrisOscuro textP"> tarjeta de crédito, débito, depósito
+            <br> bancario, pago en OXXO </span> (Mercado Pago) y con tu cuenta
+            <br> PayPal.
+            </p>
+
+        </div>
+
+        <div class="col-6" id="paddingImg">
+
+            <img src="vista/assets/img/paso3.gif" whidth="500px" height="500px">
+
+        </div>
+
+    </div>
+
+    <hr>
+
+<!--Inicio Lab/Den-->    
+
+    <div class="row" id="paddingLabDen">
+
+        <div class="col-6">
+
+            <img src="vista/assets/img/tecnicos.png" whidth="250px" height="220px">
+
+            <h2 class="pt-5">Soy un Laboratorio Dental</h2>
+
+            <p class="pt-3 textP">Sube la información de tu laboratorio y promociona tus trabajos <span class="colorGrisOscuro"> gratis. </span></p>
+
+            <p class="textP">
+                
+                Por cada orden que recibas, te cobraremos una comisión de $50. 
+                <i class="fas fa-money-bill"></i>
+            
+            </p>
+
+            <button class="btn botonLabDen">
+            
+                Crear cuenta y sube tu lista de precios <b> GRATIS </b>
+                <i class="fas fa-user-plus"></i>
+
+            </button>
+
+        </div>
+
+        <div class="col-6">
+
+            <img src="vista/assets/img/dentistas.png" whidth="250px" height="220px">
+
+            <h2 class="pt-5">Soy un Dentista</h2>
+
+            <p class="textP pt-3">Busca y cotiza el precio de cientos de Laboratorios totalmente <span class="colorGrisOscuro"> gratis.</span></p>
+
+            <p class="textP">
+                
+                Cobramos $50 de comisión por cada orden a cualquier Laboratorio.  
+                <i class="fas fa-check-circle"></i>
+            
+            </p>
+
+            <button class="btn botonLabDen">
+            
+                Crear cuenta y ordena <b> AHORA </b>
+                <i class="fas fa-arrow-right"></i>
+
+            </button>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--Pie de Pagina-->
+
+<div class="container-fluid estiloContainerPie">
+
+    <div class="row">
+
+        <div class="col-6">
+
+            <ul id="textBlancoPie">
+
+                <li>
+                    <h4>
+
+                        SERVICIO AL CLIENTE
+                        <i class="far fa-user"></i>
+
+                    </h4>
+
+                </li>
+
+                <li> <p> Preguntas frecuentes </p> </li>
+
+                <li> <p> Contacto </p> </li>
+
+                <li> <p> Marketplace </p> </li>
+
+            </ul>
+
+        </div>
+
+        <div class="col-6">
+
+            <ul id="textBlancoPie">
+
+                <li>
+                    <h4>
+                    
+                        ACERCA DE BUSCALAB
+                        <i class="far fa-question-circle"></i>
+                        
+                    </h4>
+                
+                </li>
+
+                <li> <p> ¿Quienes somos? </p> </li>
+
+                <li> <p> Formas de uso </p> </li>
+                
+                <li> <p> Politicas de Privacidad </p> </li>
+                
+                <li> <p> Politica de Cookies </p> </li>
+
+                <li> <i class="fab fa-facebook-square" id="iconRedes"></i> </li>
+
+                <li> <i class="fab fa-instagram" id="iconRedes"></i> </li>
+
+                <li> <i class="fab fa-youtube" id="iconRedes"></i> </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--Buscalab 2020-->
+
+<div class="conteiner-fluid estiloConteinerCopy">
+
+    <div class="text-center">
+
+        <h3> <b> Copyright 2020 Buscalab </b> </h3>
+
+    </div>
+
+</div>
